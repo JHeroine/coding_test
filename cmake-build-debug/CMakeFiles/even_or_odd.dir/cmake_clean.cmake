2f8011b7585e61f21b7f5fc252ae05477cbf8df7
JHeroine <dlwndus728@naver.com>
2020-05-24 19:54:05 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/even_or_odd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/even_or_odd.dir/main.cpp.o"
+  "even_or_odd"
+  "even_or_odd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/even_or_odd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
